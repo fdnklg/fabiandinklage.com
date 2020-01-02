06d2f2f1fdc41560cf11e0e3d14d9cd9043f3cba
fdnklg <fabian.dinklage@me.com>
2020-01-02 14:11:51 +0100

    build command updated

---
+++ build/public/data/data.js
@@ -0,0 +1,203 @@
+const content = {
+  "about": {
+    "intro": "Fabian Dinklage is a Berlin based designer who develops data-driven visualisations with open data as a research associate at the [Innovation & Prototyping Lab](https://lab.technologiestiftung-berlin.de/) of the Technologiestiftung Berlin. He works as a freelancer at the intersection of data and design and helped international organisations around the world to make sense of their data through visualisation.",
+    "vita": "Fabian Dinklage is a Interaction Designer with the focus of information design and data visualisation. He worked as a media designer at [ballyhoo](https://ballyhoo.de) until 2012. He started working at the [infographics group](https://inforgraphics.group) in 2016. In 2017 he took part of the interactive team of the [Berliner Morgenpost](https://berliner-morgenpost.de) as a Google News Lab Fellow.",
+    "profileUrl": "public/images/profile.jpg"
+  },
+  "cta": {
+    text: 'Do you have a project for me?',
+    btn: "Let's chat",
+  },
+  "contact": "Got curious? I'm psyched to get to know you! Let's have a coffee together or reach out via one of the following channels.",
+  "activities": [
+    {
+      "title": "Research associate",
+      "duration": "2018 – now",
+      "description": "at the [Ideation and Prototyping Lab](https://lab.technologiestiftung-berlin.de/) of the [Technologiestiftung Berlin](https://www.technologiestiftung-berlin.de/de/startseite/)"
+    },
+    {
+      "title": "Freelance",
+      "duration": "2014 – now",
+      "description": "Interaction Design & Data Visualisation for several clients and projects of all sizes"
+    },
+    {
+      "title": "Google News Lab Fellowship",
+      "duration": "2018",
+      "description": "at the Interactive Team of the [Berliner Morgenpost](https://www.morgenpost.de/interaktiv/)"
+    },
+    {
+      "title": "Interaction Design Studies",
+      "duration": "2018",
+      "description": "at the [Malmö University](http://mah.se/), Sweden"
+    },
+    {
+      "title": "Interface Design Studies",
+      "duration": "2016 – 2017",
+      "description": "at [Infographics Group](https://infographics.group/), Berlin"
+    },
+    {
+      "title": "Digital & Print Media Designer",
+      "duration": "2010 – 2013",
+      "description": "at the [ballyhoo media agency](https://www.ballyhoo.de/), Hamburg"
+    }
+  ],
+  "projects": [
+    {
+      "title": "A city in flux",
+      "path": "cityinflux",
+      "color": ['#2B1174', '#F5F3F8'],
+      "client": "Technologiestiftung Berlin",
+      "type": "data visualization",
+      "year": 2019,
+      "thumbnail": "public/images/cityinflux/cityinflux-thumb-960x640.jpg",
+      "overlay": "public/images/genderinequality/genderinequality-thumb-960x640-2.jpg",
+      "url": "https://city-in-flux.netlify.com/",
+      "tasks": ["concept", "visual interface design", "data visualization"],
+      "subtitle": "800 Routes of Diversity and Contrast",
+      "description": "»City in Flux« visualizes the informal and formal mobility networks in Cape Town. It reveals hidden patterns and dividing lines throughout the city and tells meaningful stories that help to understand the developments in the city's physical and social mobility networks.",
+      "media": [
+        {
+          "type": "img",
+          "content": "800 route of public and informal mobility routes visualized",
+          "url": "public/images/cityinflux/cityinflux-start-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "The data can be filtered by socio-economic properties such as education",
+          "url": "public/images/cityinflux/cityinflux-detail-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Along inidividual rides socio-economic transformations can be observed",
+          "url": "public/images/cityinflux/cityinflux-ride-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Unique story snippets can be attached to each route stop",
+          "url": "public/images/cityinflux/cityinflux-story-3840x2592.jpg"
+        },
+      ]
+    },
+    {
+      "title": "Gender inequality in politics",
+      "path": "genderinequality",
+      "type": "data visualization",
+      "client": "Experiment",
+      "color": ['#882C94', '#F5F3F7'],
+      "year": 2019,
+      "thumbnail": "public/images/genderinequality/genderinequality-thumb-960x640.jpg",
+      "overlay": "public/images/genderinequality/genderinequality-thumb-960x640-2.jpg",
+      "url": "https://fabiandinklage.com/projects/gender-equality/",
+      "tasks": ["data visualization"],
+      "subtitle": "Balance of gender in national parliaments visualized.",
+      "description": "Gender inequality in politics. It is a data visualization which shows the transformation of seats in national political parliaments by biological sex in one interactive graphic. The application visualizes the progress towards gender equality for each country and makes cultural differences between countries comparable through interaction",
+      "media": [
+        {
+          "type": "img",
+          "content": "Sorted by continents",
+          "url": "public/images/genderinequality/genderinequality-continents-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Sorted by progression between 1990 and 2020",
+          "url": "public/images/genderinequality/genderinequality-progress-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "North american countries highlighted",
+          "url": "public/images/genderinequality/genderinequality-northAmerica-3840x2592.jpg"
+        },
+      ]
+    },
+    {
+      "title": "Edutech Map Berlin",
+      "path": "edutech",
+      "type": "data visualization",
+      "client": "Technologiestiftung Berlin",
+      "color": ['#41B496', '#F6FCFA'],
+      "year": 2020,
+      "thumbnail": "public/images/edutech/edutech-thumb-960x640.jpg",
+      "overlay": "public/images/edutech/edutech-thumb-960x640-2.jpg",
+      "url": "https://edutech.technologiestiftung-berlin.de",
+      "tasks": ["data visualization"],
+      "subtitle": "Bike traffic in Berlin visualized",
+      "description": "Since 2013, cycle traffic has been recorded automatically throughout the city with the aid of counting stations embedded in the road. The visualization shows annual, weekly and daily driving patterns and aggregates more than 50.000 data points of 26 counting stations over the last six years in Berlin.",
+      "media": [
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/edutech/edutech-selected-3840x2040.jpg"
+        },
+      ]
+    },
+    {
+      "title": "Rhythm of the road",
+      "path": "rhythmoftheroad",
+      "type": "data visualization",
+      "client": "Technologiestiftung Berlin",
+      "color": ['#2824B2', '#F5F4FC'],
+      "year": 2018,
+      "thumbnail": "public/images/rhythmoftheroad/rhythmoftheroad-thumb-960x640.jpg",
+      "overlay": "public/images/genderinequality/genderinequality-thumb-960x640-2.jpg",
+      "url": "https://lab.technologiestiftung-berlin.de/projects/bikerides/de/",
+      "tasks": ["data visualization"],
+      "subtitle": "Bike traffic in Berlin visualized",
+      "description": "Since 2013, cycle traffic has been recorded automatically throughout the city with the aid of counting stations embedded in the road. The visualization shows annual, weekly and daily driving patterns and aggregates more than 50.000 data points of 26 counting stations over the last six years in Berlin.",
+      "media": [
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/rhythmoftheroad/rhythmoftheroad-pattern-3840x2246.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Grid aligned layout of all bike counting stations",
+          "url": "public/images/rhythmoftheroad/rhythmoftheroad-overview-3840x2246.jpg"
+        },
+      ]
+    },
+    {
+      "title": "Mobility Sharing Flows",
+      "path": "bikesharing",
+      "type": "data visualization",
+      "client": "Technologiestiftung Berlin",
+      "color": ['#EE7B4B', '#fff'],
+      "year": 2019,
+      "thumbnail": "public/images/bikesharing/bikesharing-thumb-960x640.jpg",
+      "overlay": "public/images/genderinequality/genderinequality-thumb-960x640-2.jpg",
+      "url": "https://bikesharing.citylab-berlin.de",
+      "tasks": ["data visualization"],
+      "subtitle": "Bike traffic in Berlin visualized",
+      "description": "Since 2013, cycle traffic has been recorded automatically throughout the city with the aid of counting stations embedded in the road. The visualization shows annual, weekly and daily driving patterns and aggregates more than 50.000 data points of 26 counting stations over the last six years in Berlin.",
+      "media": [
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/bikesharing/bikesharing-overview-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/bikesharing/bikesharing-district-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/bikesharing/bikesharing-detail-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/bikesharing/bikesharing-bikestreets-3840x2592.jpg"
+        },
+        {
+          "type": "img",
+          "content": "Daily, weekly and monthly analysis of Berlin's cyclists",
+          "url": "public/images/bikesharing/bikesharing-ui-3840x2592.jpg"
+        },
+      ]
+    },
+  ]
+}
+
+export default content;
